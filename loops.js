9ac026addc65f137841483e57be9c4822a593963
jemsley canela <jemscanela@gmail.com>
2018-05-30 16:25:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,10 +3,10 @@ function forLoop(array){
   for (var i =0; i<25; i++){
     if(i===1){
       
-      array.push("i am 1 strange loop")
+      array.unshift("i am 1 strange loop")
     }
     else{
-      array.push("i am ${i} strange loops")
+      array.unshift("i am ${i} strange loops")
     }
     
     
